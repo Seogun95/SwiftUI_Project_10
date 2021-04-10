fbc547c87a8ae0aa5e3e8923cda4a059d22f07d9
SeonJungKim <ksj083895@gmail.com>
2021-04-10 15:15:45 +0500

    Initial Commit

---
+++ SwiftUI_Project_10/SwiftUI_Project_10App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Project_10App.swift
+//  SwiftUI_Project_10
+//
+//  Created by 김선중 on 2021/04/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Project_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
